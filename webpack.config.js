9537192de7db02c4699f1f1c2032befdaf349398
Frankie Xiao <fxiaoX089@live.com>
2023-01-30 17:44:33 -0600

    Add splash.html page

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: {
+        main: "./src/index.js",
+        splash: "./src/index.js",
+    },
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
@@ -37,8 +40,15 @@ module.exports = {
 
     plugins: [
         new HtmlWebpackPlugin({
-            title: "Output Management",
-            template: "./src/template.html",
+            filename: "index.html",
+            template: "./src/html/main.html",
+            chunks: ["main"],
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: "splash.html",
+            template: "./src/html/splash.html",
+            chunks: ["splash-page"],
         }),
     ],
 }
